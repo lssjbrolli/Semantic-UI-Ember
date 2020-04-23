2bcea69fc804b542163e00f15e3a207d94fbd44f
Sergiu Cozma <lssjbrolli@gmail.com>
2020-04-23 20:06:14 +0300

    WIP: Octane update: glimmer components:  - create base checkbox component  - update checkbox

--- tests/integration/components/ui-checkbox-test.js
+++ tests/integration/components/ui-checkbox-test.js
@@ -3,10 +3,10 @@ import { setupRenderingTest } from "ember-qunit";
 import { render, click } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
 
-module("Integration | Component | ui checkbox", function(hooks) {
+module("Integration | Component | ui checkbox", function (hooks) {
   setupRenderingTest(hooks);
 
-  test("it renders", async function(assert) {
+  test("it renders", async function (assert) {
     assert.expect(2);
 
     let count = 0;
@@ -23,11 +23,11 @@ module("Integration | Component | ui checkbox", function(hooks) {
     assert.equal(count, 0, "onChange should not have been called");
   });
 
-  test("checking will update the bound property", async function(assert) {
+  test("checking will update the bound property", async function (assert) {
     assert.expect(3);
 
     let count = 0;
-    this.set("changed", value => {
+    this.set("changed", (value) => {
       this.set("checked", value);
       count++;
     });
@@ -43,11 +43,11 @@ module("Integration | Component | ui checkbox", function(hooks) {
     assert.equal(count, 1, "onChange should have only been called once");
   });
 
-  test("setting disabled ignores click", async function(assert) {
+  test("setting disabled ignores click", async function (assert) {
     assert.expect(4);
 
     let count = 0;
-    this.set("changed", value => {
+    this.set("changed", (value) => {
       this.set("checked", value);
       count++;
     });
@@ -62,17 +62,19 @@ module("Integration | Component | ui checkbox", function(hooks) {
     await click(".ui.checkbox");
     assert.equal(false, this.checked);
 
+    // await this.pauseTest();
+
     this.set("disabled", false);
     await click(".ui.checkbox");
     assert.equal(true, this.checked);
     assert.equal(count, 1, "onChange should have only been called once");
   });
 
-  test("setting readonly ignores click", async function(assert) {
+  test("setting readonly ignores click", async function (assert) {
     assert.expect(4);
 
     let count = 0;
-    this.set("changed", value => {
+    this.set("changed", (value) => {
       this.set("checked", value);
       count++;
     });
